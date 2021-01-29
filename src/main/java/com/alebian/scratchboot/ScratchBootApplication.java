fb96fa173071336e11de215f4def77816f29f182
Alejandro Bezdjian <alebezdjian@gmail.com>
2021-01-29 17:50:49 -0300

    Initial commit

---
+++ src/main/java/com/alebian/scratchboot/ScratchBootApplication.java
@@ -0,0 +1,14 @@
+package com.alebian.scratchboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application entry point.
+ */
+@SpringBootApplication
+public class ScratchBootApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ScratchBootApplication.class, args);
+  }
+}
---
+++ src/test/java/com/alebian/scratchboot/ScratchBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alebian.scratchboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScratchBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
